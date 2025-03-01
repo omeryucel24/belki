5255aa94b9c8661de3f4368f313c8218e765efb6
omeryucel24 <yucelomeremre2@gmail.com>
2025-03-01 11:15:06 +0300

    Update öylesine.py

--- öylesine.py
+++ öylesine.py
@@ -1,15 +1,18 @@
 import time
-while True:
-    meme_dict = {
+meme_dict = {
                 "CRINGE": "Garip ya da utandırıcı bir şey",
                 "LOL": "Komik bir şeye verilen cevap",
                 }
-    
+while True:
     word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+    if word=="EXİT":
+        print("çıkış yapılıyor...")
+        break
     if word in meme_dict.keys():
         print("aradığınız kelime",word,meme_dict[word])
-               
+        time.sleep(1)
     else:
         print("aradığınız kelime bulunmamaktadır.")
+        time.sleep(1)
         
         # Kelime eşleşmiyorsa ne yapmalıyız?
